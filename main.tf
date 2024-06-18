af63a33dbd9d12a28060d688941d3d922454b038
OlafHappyson <95042777+OlafHappyson@users.noreply.github.com>
2024-06-18 20:26:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,7 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "s3_bucket" {}
-variable "s3_key" {}
-variable "s3_region" {}
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.s3_region
-}
-
 terraform {
   backend "s3" {
-    bucket         = var.s3_bucket
-    key            = var.s3_key
-    region         = eu-central-1
+    bucket         = "generalpurpose-statefile-robotdreams"
+    key            = "path/to/terraform.tfstate"
+    region         = "eu-central-1"
   }
 }
