3ee5220902f41c4e0caf31283874609c1a497168
OlafHappyson <95042777+OlafHappyson@users.noreply.github.com>
2024-06-18 20:39:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,35 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+# Private S3 Bucket
 resource "aws_s3_bucket" "private_bucket" {
-  bucket = "generalpurpose-private-robotdreams"
-  acl    = "private"
+  bucket = "my-private-bucket-example"
 }
 
+resource "aws_s3_bucket_policy" "private_bucket_policy" {
+  bucket = aws_s3_bucket.private_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Deny"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.private_bucket.arn}/*"
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Public S3 Bucket
 resource "aws_s3_bucket" "public_bucket" {
-  bucket = "generalpurpose-public-robotdreams"
-  acl    = "public-read"
+  bucket = "my-public-bucket-example"
 }
 
 resource "aws_s3_bucket_policy" "public_bucket_policy" {
