358fa63c6aef472edb12fa55bdc09d72790fb087
OlafHappyson <95042777+OlafHappyson@users.noreply.github.com>
2024-06-18 20:15:36 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "s3_bucket" {}
+variable "s3_key" {}
+variable "s3_region" {}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.s3_region
+}
+
+terraform {
+  backend "s3" {
+    bucket         = var.s3_bucket
+    key            = var.s3_key
+    region         = var.s3_region
+  }
+}
