62bc6840929b1793a5bc6fdf8029ef4934dbbad8
OlafHappyson <95042777+OlafHappyson@users.noreply.github.com>
2024-06-18 21:03:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,54 +7,19 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-central-1"
+  region = "us-west-2"
 }
 
 # Private S3 Bucket
 resource "aws_s3_bucket" "private_bucket" {
   bucket = "generalpurpose-private-robotdreams"
-}
-
-resource "aws_s3_bucket_policy" "private_bucket_policy" {
-  bucket = aws_s3_bucket.private_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Deny"
-        Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.private_bucket.arn}/*"
-        Condition = {
-          Bool = {
-            "aws:SecureTransport" = "false"
-          }
-        }
-      }
-    ]
-  })
+  acl    = "private"
 }
 
 # Public S3 Bucket
 resource "aws_s3_bucket" "public_bucket" {
   bucket = "generalpurpose-public-robotdreams"
-}
-
-resource "aws_s3_bucket_policy" "public_bucket_policy" {
-  bucket = aws_s3_bucket.public_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.public_bucket.arn}/*"
-      }
-    ]
-  })
+  acl    = "public-read"
 }
 
 output "private_bucket_name" {
